02e9d88679ac3511aeeaa54e9070d19cf05905f7
German <germanstarinenko@gmail.com>
2023-05-11 17:48:24 +0300

    1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,24 @@
--- SQL-команды для создания таблиц
+create table employees
+(
+ first_name varchar(50) not NULL,
+ last_name varchar(50) not NULL,
+ title varchar(50) not NULL,
+ birth_date date,
+ notes text
+);
+
+create table customers
+(
+customer_id varchar(50) ,
+company_name text,
+contact_name text
+);
+
+create table orders
+(
+order_id int primary key,
+customer_id varchar(50) REFERENCES orders(customer_id),
+employee_id int,
+order_date date,
+ship_city varchar(50) not NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,36 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2, csv
+
+conn = psycopg2.connect(host="localhost", database="north", user="postgres", password=None)
+
+cursor = conn.cursor()
+
+with open('north_data/employees_data.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader)  # пропускаем заголовок
+    for row in reader:
+        cursor.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s)", row)
+
+with open('north_data/customers_data.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        cursor.execute("INSERT INTO customers  VALUES (%s, %s, %s)", row)
+
+with open('north_data/orders_data.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        if row == "psycopg2.errors.UniqueViolation: ОШИБКА:":
+            pass
+        else:
+            cursor.execute("INSERT INTO orders  VALUES (%s, %s, %s, %s, %s)", row)
+
+conn.commit()
+conn.close()
+
+
+
+
+
+
+
